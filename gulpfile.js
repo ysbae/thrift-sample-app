80a6b3a28fe32729a158b4feaf5ef469ca209058
Hyungsul Kim <hyungsul.kim@meshkorea.net>
2016-02-02 18:29:51 +0900

    Set up bower package dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,12 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+
+    mix.scripts([
+        '../bower/jquery/jquery.js',
+        '../bower/thrift/lib/js/src/thrift.js',
+        '../bower/thrift-sample/dist-js/thrift-sample.js'
+    ], 'public/js/vendor.js');
+
+    mix.scripts(['../js/main.js'], 'public/js/main.js');
 });
